921b3601b7719c207c934b7bfcae7f9450ac52bd
timothy kemboi <tkemboi889@gmail.com>
2025-12-06 10:35:47 +0300

    add created models to the project

--- DBMS/edairy/admin.py
+++ DBMS/edairy/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
-
+from .models import Owner,Milk,Calf,Feed,Worker,Cow
 # Register your models here.
+admin.site.register(Owner)
+admin.site.register(Milk)
+admin.site.register(Calf)
+admin.site.register(Feed)
+admin.site.register(Worker)
+admin.site.register(Cow)
--- DBMS/edairy/models.py
+++ DBMS/edairy/models.py
@@ -1,3 +1,59 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Owner(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    phone_number = models.CharField(max_length=15, null=True, blank=True)
+
+    def __str__(self):
+        return self.user.username
+
+
+class Cow(models.Model):
+    name = models.CharField(max_length=50)
+    breed = models.CharField(max_length=20)
+    sex = models.CharField(choices=[("M","Male"),("F","Female")],max_length=20)
+    age = models.IntegerField()
+    owner = models.ForeignKey(Owner, on_delete=models.CASCADE)
+    
+
+    def __str__(self):
+        return self.name
+
+
+class Calf(models.Model):
+    name = models.CharField(max_length=20)
+    breed = models.CharField(max_length=20)
+    sex = models.CharField(choices=[("M","Male"),("F","Female")],max_length=20)
+    age = models.IntegerField()
+    cow = models.ForeignKey(Cow, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
+
+class Milk(models.Model):
+    amount_in_litres = models.FloatField()
+    date_collected = models.DateField(auto_now_add=True)
+    cow = models.ForeignKey(Cow, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.amount_in_litres} L from {self.cow.name}"
+
+
+class Feed(models.Model):
+    name = models.CharField(max_length=20)
+    store_number = models.IntegerField()
+    cow = models.ForeignKey(Cow, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
+
+class Worker(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    phone_number = models.CharField(max_length=15)
+    cow = models.ManyToManyField(Cow,blank=True )
+
+    def __str__(self):
+        return self.user.username
